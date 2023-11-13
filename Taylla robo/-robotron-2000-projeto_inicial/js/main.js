5983b6c65cebb8fd0a18acf8373a65286a40d8fb
Taylla-Lima <119326235+Taylla-Lima@users.noreply.github.com>
2023-11-13 08:52:47 -0300

    Add files via upload

---
+++ Taylla robo/-robotron-2000-projeto_inicial/js/main.js
@@ -0,0 +1,11 @@
+const robotron = document.querySelector("#robotron")
+
+robotron.addEventListener("click", () => 
+console.log(
+
+function dizOi() {
+    console.log("oi")
+    console.log("Bem-vindo ao Robotron 2000")
+}
+
+dizOi()
