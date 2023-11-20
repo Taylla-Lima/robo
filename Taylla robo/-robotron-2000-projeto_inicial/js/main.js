3b13780a7361338baf8b632909a580342b8859fd
Taylla-Lima <119326235+Taylla-Lima@users.noreply.github.com>
2023-11-20 08:53:57 -0300

    Add files via upload

--- Taylla robo/-robotron-2000-projeto_inicial/js/main.js
+++ Taylla robo/-robotron-2000-projeto_inicial/js/main.js
@@ -1,11 +1,21 @@
-const robotron = document.querySelector("#robotron")
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
 
-robotron.addEventListener("click", () => 
-console.log(
+const console = document.querySelectorAll(".console-ajuste")
+console.log(controle)
 
-function dizOi() {
-    console.log("oi")
-    console.log("Bem-vindo ao Robotron 2000")
+somar.addEventListener("click", () => {manipulaDados("somar")})
+
+
+subtrair.addEventListener("click", () => {manipulaDados("subtrair")})
+    
+
+function manipulaDados(operacao) {
+    if(operacao === "subtrair") {
+        braco.value = parseInt(braco.value) - 1
+} else {
+    braco.value = parseInt(braco.value) + 1
 }
-
-dizOi()
+    }
+}
